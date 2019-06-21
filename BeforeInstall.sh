c483528980c6b8ef42e370a2a412b07aba320987
rahulbhatu <40614613+rahulbhatu@users.noreply.github.com>
2019-06-21 15:52:06 +0530

    Delete BeforeInstall.sh

--- BeforeInstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#su - ubuntu -c "/usr/bin/pm2 stop all"
-su - ubuntu -c "/usr/bin/pm2 stop all"
-su - ubuntu -c "/usr/bin/pm2 delete all"
-sleep 5
-pkill -f node
-rm -rf /home/ubuntu/cicd-pipeline-train-schedule-cd/
-mkdir -p /home/ubuntu/cicd-pipeline-train-schedule-cd/
