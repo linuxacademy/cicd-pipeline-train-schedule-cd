da81cbcd913214e709838605427bf3dc09bfb547
rahulbhatu <40614613+rahulbhatu@users.noreply.github.com>
2019-06-21 15:52:28 +0530

    Add files via upload

---
+++ BeforeInstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+su - ubuntu -c "/usr/bin/pm2 stop all"
+su - ubuntu -c "/usr/bin/pm2 delete all"
+sleep 5
+pkill -f node
+rm -rf /home/ubuntu/cicd-pipeline-train-schedule-cd/
+mkdir -p /home/ubuntu/cicd-pipeline-train-schedule-cd/
